eb3ed475130edd84f61e75e04d6bbcff4b6031f8
Nathan Park <nalhan.park@gmail.com>
2024-08-22 04:32:34 +0000

    add support for linting on github

---
+++ mitplan-frontend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    // Add any custom rules here
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
